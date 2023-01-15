03adb90645302b4005a8b8c50b874989db4df7d0
hangyeolhong <59331040+hangyeolhong@users.noreply.github.com>
2023-01-15 22:15:45 +0900

    Create Combination Sum.py

---
+++ leetcode/Backtracking/Combination Sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.answer = []
+        self.candidates = candidates
+
+        self.candidates.sort()
+        self.dfs([], 0, target)
+
+        return self.answer
+
+
+    def dfs(self, res, idx, target):
+
+        if idx == len(self.candidates):
+            return
+        
+        if sum(res) == target:
+            self.answer.append(res)
+            return
+        
+        for i in range(idx, len(self.candidates)):
+            if sum(res) + self.candidates[i] <= target:
+                self.dfs(res + [self.candidates[i]], i, target)
+        
