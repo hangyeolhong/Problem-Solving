5e8bf98a2cf17ecacef319079c4c603374d16627
hangyeolhong <59331040+hangyeolhong@users.noreply.github.com>
2023-01-15 21:52:32 +0900

    Update Product of Array Except Self.py

--- leetcode/Product of Array Except Self.py
+++ leetcode/Product of Array Except Self.py
@@ -1,5 +1,10 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+            pref[i]: 0 ~ (i-1)th 인덱스까지 누적 곱
+            suffix[i]: (i+1) ~ nums 마지막 인덱스까지 누적 곱
+        """
+        
         pref = [1]
 
         for i in range(1, len(nums)):
