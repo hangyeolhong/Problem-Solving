5bf78bd71329fd33f3dd2fddd557eca81f23d932
hangyeolhong <59331040+hangyeolhong@users.noreply.github.com>
2023-01-15 22:40:06 +0900

    Create Combination Sum III.py

---
+++ leetcode/Backtracking/Combination Sum III.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        self.answer = []
+        self.dfs(1, [], k, n)
+        return self.answer
+        
+
+    def dfs(self, idx, res, k, n):
+        if len(res) == k:
+            if sum(res) == n:
+                self.answer.append(res)
+            return
+
+        if idx == 10:
+            return
+
+        for i in range(idx, 10):
+            if sum(res) + i <= n:
+                self.dfs(i + 1, res + [i], k, n)
