236e2d9f5b76ba9e639fc51d60f05d44568344db
hangyeolhong <59331040+hangyeolhong@users.noreply.github.com>
2023-01-19 17:23:02 +0900

    Create Search Insert Position.py

---
+++ leetcode/Binary Search/Search Insert Position.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        # binary search: sorted array
+
+        left, right = 0, len(nums) - 1
+
+        answer = -1
+
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                answer = mid
+                break
+            
+            elif nums[mid] < target:
+                answer = mid + 1  # insert at next position (mid + 1)
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return answer
