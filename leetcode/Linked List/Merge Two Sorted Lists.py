d9d93c9c096b5b518a93d700dfba0a5912b5e16e
hangyeolhong <59331040+hangyeolhong@users.noreply.github.com>
2023-01-20 17:11:57 +0900

    Update Merge Two Sorted Lists.py

--- leetcode/Linked List/Merge Two Sorted Lists.py
+++ leetcode/Linked List/Merge Two Sorted Lists.py
@@ -14,13 +14,13 @@ class Solution:
 
         while list1 and list2:
             if list1.val < list2.val:
-                cur.next = list1
+                cur.next = list1        # dummy also changes
                 list1 = list1.next
 
             else:
-                cur.next = list2
+                cur.next = list2        # dummy also changes
                 list2 = list2.next
-            cur = cur.next
+            cur = cur.next              # reassign cur -> cur points to new object 
 
         if list1 or list2:
             cur.next = list1 if list1 else list2
