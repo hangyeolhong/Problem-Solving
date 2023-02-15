75063208144b66e5ef8d932fa4dd100576b81161
hangyeolhong <59331040+hangyeolhong@users.noreply.github.com>
2023-02-16 02:22:33 +0900

    Create Subsets.py

---
+++ leetcode/Backtracking/Subsets.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        answer = []
+
+        res = []
+
+        def dfs(i):
+            if i >= len(nums):
+                answer.append(res[::])
+                return
+
+            # include nums[i]
+            res.append(nums[i])
+            dfs(i + 1)
+
+            # not include nums[i] -> pop the element that we just appended
+            res.pop()
+            dfs(i + 1)
+
+        dfs(0)
+
+        return answer
