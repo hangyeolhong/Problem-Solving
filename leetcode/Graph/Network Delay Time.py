07c2ba43a79bf69d4ae882a15759215b8d6cd857
hangyeolhong <59331040+hangyeolhong@users.noreply.github.com>
2023-02-19 22:55:36 +0900

    Create Network Delay Time.py

---
+++ leetcode/Graph/Network Delay Time.py
@@ -0,0 +1,32 @@
+import heapq
+from collections import defaultdict
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        INF = int(1e9)
+        dist = [INF] * n    # one-indexed
+        dist[k - 1] = 0     # start point
+
+        # directed edges
+        adj = defaultdict(list)
+
+        for time in times:
+            u, v, w = time
+            adj[u - 1].append([v - 1, w])
+
+        q = []
+        heapq.heappush(q, [0, k - 1])   # [dist, zero-indexed node number]
+
+        while q:
+            d, cur = heapq.heappop(q)
+
+            if dist[cur] < d:
+                continue
+
+            for neighbor, cost in adj[cur]:
+                if cost + d < dist[neighbor]:
+                    dist[neighbor] = cost + d
+                    heapq.heappush(q, [dist[neighbor], neighbor])
+
+        return -1 if INF in dist else max(dist)
