2b4494799b6587d3320ecbbac686a8c11624d39a
hangyeolhong <59331040+hangyeolhong@users.noreply.github.com>
2023-02-28 17:20:34 +0900

    Create Longest Repeating Character Replacement.py

---
+++ leetcode/Sliding Window/Longest Repeating Character Replacement.py
@@ -0,0 +1,19 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        cnt = dict()
+
+        l = 0
+        maxf = -1
+        res = 0
+
+        for r in range(len(s)):
+            cnt[s[r]] = cnt.get(s[r], 0) + 1
+            maxf = max(maxf, cnt[s[r]])
+
+            if (r - l + 1) - maxf > k:
+                cnt[s[l]] -= 1
+                l += 1
+            
+            res = max(res, r - l + 1)
+
+        return res
