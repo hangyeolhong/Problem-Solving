1a553099a85912e34a086ae1446adce9c96fafb4
hangyeolhong <59331040+hangyeolhong@users.noreply.github.com>
2023-03-01 14:46:11 +0900

    Update 787. Cheapest Flights Within K Stops.py

--- leetcode/Graph/787. Cheapest Flights Within K Stops.py
+++ leetcode/Graph/787. Cheapest Flights Within K Stops.py
@@ -2,17 +2,18 @@
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        
-        prices = [float("inf")] * n
+        INF = int(1e9)
+        prices = [INF] * n
         prices[src] = 0
 
         for i in range(k + 1):
             tmpPrices = prices.copy()
 
             for s, d, p in flights:  # s=source, d=dest, p=price
-                if prices[s] == float("inf"):
+                if prices[s] == INF:
                     continue
                 if prices[s] + p < tmpPrices[d]:
                     tmpPrices[d] = prices[s] + p
             prices = tmpPrices
-        return -1 if prices[dst] == float("inf") else prices[dst]
+            
+        return -1 if prices[dst] == INF else prices[dst]
