09bdc952f666cc1f71a6ded630755febe6fdd7f1
hangyeolhong <59331040+hangyeolhong@users.noreply.github.com>
2023-03-04 11:25:53 +0900

    Update Course Schedule II.py

--- leetcode/Graph/Course Schedule II.py
+++ leetcode/Graph/Course Schedule II.py
@@ -1,5 +1,5 @@
+#1. topological sort
 from collections import deque
-
 
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
@@ -26,12 +26,42 @@ class Solution:
 
             for neighbor in edges[x]:
                 indegree[neighbor] -= 1
-
                 if indegree[neighbor] == 0:
                     q.append(neighbor)
 
+        return answer if len(answer) == numCourses else []
 
-        if len(answer) == numCourses:
-            return answer
-        else:
-            return []   # empty array    
+#2. dfs
+from collections import defaultdict
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        preMap = defaultdict(list)
+
+        for crs, pre in prerequisites:
+            preMap[pre].append(crs)
+
+        output = []
+        visited, cycle = set(), set()
+
+        def dfs(cur_crs):
+            if cur_crs in cycle:
+                return False
+            if cur_crs in visited:
+                return True
+
+            cycle.add(cur_crs)
+            for next_crs in preMap[cur_crs]:
+                if not dfs(next_crs):
+                    # if it's cycle
+                    return False
+            cycle.remove(cur_crs)
+            visited.add(cur_crs)
+            output.append(cur_crs)
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return []
+                
+        return output[::-1]
